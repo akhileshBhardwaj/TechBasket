8c1e47a9332240eca8df6a125bee96081b9ccf16
akhileshBhardwaj <rajbharakhilesh818@gmail.com>
2026-07-16 14:15:55 +0530

    final

--- src/components/homeComponent/DiscountBanner.jsx
+++ src/components/homeComponent/DiscountBanner.jsx
@@ -22,9 +22,9 @@ const DiscountBanner = () => {
         {/* Second banner */}
          <div className=" relative">
           <img src={banner2} alt="Banner 1" className="w-full rounded-xl" />
-          <div className=" absolute top-1 md:top-1/4 left-4">
+          <div className=" absolute top-3 md:top-1/4 left-4">
             <h1 className=" text-xl font-medium md:text-3xl md:font-semibold">Fruits & Vegetables</h1>
-            <p className="text-gray-500 font-light mt-1 md:text-2xl md:font-medium md:mt-2 ">
+            <p className="text-gray-500 font-light md:text-2xl md:font-medium md:mt-2 ">
                 Get Upto <span className="text-emerald-500">30% </span> Off
             </p>
             <button className="py-1 px-2 mt-1.5 md:mt-3 md:py-2 md:px-4 bg-black text-white rounded">Shop Now</button>
